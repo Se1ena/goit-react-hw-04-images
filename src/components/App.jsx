037706535431b427efb4088672d584928262ce03
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2023-02-13 21:33:07 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,8 @@ export const App = () => {
   const [modalImg, setModalImg] = useState('');
   const [galleryItems, setGalleryItems] = useState([]);
   const [status, setStatus] = useState('start');
+  const [objectCount, setObjectCount] = useState(0);
+
 
   useEffect(() => {
     if (search === '') {
@@ -26,13 +28,10 @@ export const App = () => {
       axios.defaults.baseURL = 'https://pixabay.com/api';
       try {
         setStatus('loading');
-        const res = await axios.get('/?key=31381187-b432f12d58670729bf0a21c9a');
-        if (!search) {
-          return;
-        }
+        const res = await axios.get(`/?key=31381187-b432f12d58670729bf0a21c9a&per_page=12&q=${search}&page=${page}`);
         if (page === 1) {
           toast.info(`Hooray! We found ${res.data.total} image(s).`);
-          //calculateTotalPages(res.data.total);
+          setObjectCount(res.data.total);
         }
         if (res.data.total === 0) {
           throw new Error('Images with your querry was not found');
@@ -42,7 +41,6 @@ export const App = () => {
           ...getGalleryItems(res.data.hits),
         ]);
         setStatus('loaded');
-        //this.objectCount(res.data.total);
       } catch (error) {
         toast.error(error.message, {
           position: 'top-center',
@@ -61,9 +59,9 @@ export const App = () => {
   }, [search, page]);
 
   const handleFormSubmit = value => {
-    setSearch(value.search);
     setGalleryItems([]);
     setPage(1);
+    setSearch(value.search);    
   };
 
   const handleModal = image => {
@@ -78,10 +76,6 @@ export const App = () => {
     }));
   };
 
-  //const calculateTotalPages = (total) => {
-  //this({ totalPages: Math.ceil(total / 12) });
-  //};
-
   const loadMore = () => {
     setPage(prevState => prevState + 1);
   };
@@ -93,113 +87,9 @@ export const App = () => {
         <ImageGallery galleryItems={galleryItems} onClick={handleModal} />
       )}
       {status === 'loading' && <Loader />}
-      {status === 'loaded' && <Button loadMore={loadMore} />}
+      {status === 'loaded' && objectCount !== galleryItems.length && <Button loadMore={loadMore} />}
       {modalImg && <Modal image={modalImg} onModalClose={handleModal} />}
       <ToastContainer />
     </Wrapper>
   );
 };
-
-//export class App extends Component {
-//state = {
-//page: 1,
-//search: '',
-//modalImg: '',
-//galleryItems: [],
-//status: 'start',
-//};
-
-//async componentDidUpdate(prevProps, prevState) {
-//const { search, page } = this.state;
-//try {
-//if (prevState.search !== search || prevState.page !== page) {
-//this.setState({ status: 'loading' });
-//const res = await fetchImages(search, page);
-//if (!search) {
-//return;
-//}
-//if (page === 1) {
-//toast.info(`Hooray! We found ${res.data.total} image(s).`);
-//this.calculateTotalPages(res.data.total);
-//}
-//if (res.data.total === 0) {
-//throw new Error('Images with your querry was not found');
-//}
-//this.setState(prevState => ({
-//galleryItems: [
-//...prevState.galleryItems,
-//...this.getGalleryItems(res.data.hits),
-//],
-//status: 'loaded',
-//objectCount: res.data.total,
-//}));
-//}
-//} catch (error) {
-//toast.error(error.message, {
-//position: 'top-center',
-//autoClose: 2500,
-//hideProgressBar: true,
-//closeOnClick: true,
-//pauseOnHover: true,
-//draggable: true,
-//progress: undefined,
-//theme: 'colored',
-//});
-//this.setState({ status: 'start' });
-//}
-//}
-
-//handleFormSubmit = value => {
-//this.setState({
-//search: value.search,
-//galleryItems: [],
-//page: 1,
-//});
-//};
-
-//loadMore = () => {
-//this.setState(prevState => ({ page: prevState.page + 1 }));
-//};
-
-//handleModal = image => {
-//this.setState({ modalImg: image });
-//};
-
-//getGalleryItems = data => {
-//return data.map(el => ({
-//id: el.id,
-//webformatURL: el.webformatURL,
-//largeImageURL: el.largeImageURL,
-//}));
-//};
-
-//calculateTotalPages(total) {
-//this.setState({ totalPages: Math.ceil(total / 12) });
-//}
-
-//render() {
-//const { status, modalImg, galleryItems, objectCount } = this.state;
-
-//return (
-//<Wrapper>
-//<Searchbar onSubmit={this.handleFormSubmit} />
-//{galleryItems.length > 0 && (
-//<ImageGallery
-//galleryItems={galleryItems}
-//onClick={this.handleModal}
-///>
-//)}
-//{status === 'loading' && <Loader />}
-
-//{status === 'loaded' &&
-//(galleryItems.length < objectCount ? (
-//<Button loadMore={this.loadMore} />
-//) : (
-//<span>Nothing to load</span>
-//))}
-//{modalImg && <Modal image={modalImg} onModalClose={this.handleModal} />}
-//<ToastContainer />
-//</Wrapper>
-//);
-//}
-//}
